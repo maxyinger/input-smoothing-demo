67325ba108effa3836be341b8aa032d09c3e61a0
Maxwell Yinger <max@Maxwells-MacBook-Pro.local>
2019-08-15 14:51:33 -0600

    Updates

--- precache-manifest.a4f241ad0a51b0e14dfb465f0d9c6dab.js
+++ precache-manifest.a4f241ad0a51b0e14dfb465f0d9c6dab.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6162795586d333848bbbffde3ddcfff5",
+    "revision": "a374485ad8a8b0121f354bf6f0dfa7e2",
     "url": "/input-smoothing-demo/index.html"
   },
   {
-    "revision": "04ceb4f3bea18b0e2f95",
+    "revision": "2beb4eebaee30dd12dd3",
     "url": "/input-smoothing-demo/static/css/main.7a5b53ea.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/input-smoothing-demo/static/js/2.b2b16714.chunk.js"
   },
   {
-    "revision": "04ceb4f3bea18b0e2f95",
-    "url": "/input-smoothing-demo/static/js/main.3fe6b770.chunk.js"
+    "revision": "2beb4eebaee30dd12dd3",
+    "url": "/input-smoothing-demo/static/js/main.0f4bcc21.chunk.js"
   },
   {
     "revision": "f3eb8baa581e5cb916f7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/input-smoothing-demo/precache-manifest.2537f39d021a712419683f8b90274bba.js"
+  "/input-smoothing-demo/precache-manifest.a4f241ad0a51b0e14dfb465f0d9c6dab.js"
 );
 
 self.addEventListener('message', (event) => {
